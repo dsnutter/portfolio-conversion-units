110c78a71ee8d81a828f5c107016576c6bd88e07
DS Nutter <11504386+dsnutter@users.noreply.github.com>
2023-11-16 15:02:06 -0500

    Added special case data for absolute zero

--- tests/unit/test_conversion_vm.py
+++ tests/unit/test_conversion_vm.py
@@ -14,91 +14,91 @@ class Test_Conversion_VM:
                 ("x / 2", 8, 4),
                 ("x - 2", 4, 2),
                 #temperature,Farenheit,Celsius
-                ("(x-32.0) * (5/9)", 32, 0),
+                ("(x-32.0) * (5/9)", 100, 0),
                 #temperature,Farenheit,Kelvin
-                ("((x-32.0) * (5/9)) + 273.15", 0, 0),
+                ("((x-32.0) * (5/9)) + 273.15", 100.0, 0),
                 #temperature,Farenheit,Rankine
-                ("x + 459.67", 0, 0),
+                ("x + 459.67", 100.0, 0),
                 #temperature,Celsius,Farenheit
-                ("(x * (9/5)) + 32.0", 0, 32),
+                ("(x * (9/5)) + 32.0", 100.0, 32),
                 #temperature,Celsius,Kelvin
-                ("x + 273.15", 0, 0),
+                ("x + 273.15", 100.0, 0),
                 #temperature,Celsius,Rankine
-                ("(x + 273.15) * 9/5", 0, 0),
+                ("(x + 273.15) * 9/5", 100.0, 0),
                 #temperature,Kelvin,Farenheit
-                ("((x-273.15) * (9/5)) + 32.0", 0, 0),
+                ("((x-273.15) * (9/5)) + 32.0", 100.0, 0),
                 #temperature,Kelvin,Celsius
-                ("x - 273.15", 0, 0),
+                ("x - 273.15", 100, 0),
                 #temperature,Kelvin,Rankine
-                ("x * 9/5", 0, 0),
+                ("x * 9/5", 100, 0),
                 #temperature,Rankine,Farenheit
-                ("x - 459.67", 0, 0),
+                ("x - 459.67", 100, 0),
                 #temperature,Rankine,Celsius
-                ("(x * 5/9) - 273.15", 0, 0),
+                ("(x * 5/9) - 273.15", 100, 0),
                 #temperature,Rankine,Kelvin
-                ("x * 5/9", 0, 0),
+                ("x * 5/9", 100, 0),
                 #temperature,ThinkDifferent,Kelvin
-                ("x + 1", 0, 0),
+                ("x + 1", 100, 0),
                 #volume,Liters,Tablespoons
-                ("x * (67.0 + 2/3)", 0, 0),
+                ("x * (67.0 + 2/3)", 100, 0),
                 #volume,Liters,Cubic_inches
-                ("x * 61.023744", 0, 0),
+                ("x * 61.023744", 100, 0),
                 #volume,Liters,Cups
-                ("x * 4.2267528377", 0, 0),
+                ("x * 4.2267528377", 100, 0),
                 #volume,Liters,Cubic_feet
-                ("x * 0.035315", 0, 0),
+                ("x * 0.035315", 100, 0),
                 #volume,Liters,Gallons
-                ("x * 0.264172", 0, 0),
+                ("x * 0.264172", 100, 0),
                 #volume,Tablespoons,Liters
-                ("x / (67.0 + 2/3)", 0, 0),
+                ("x / (67.0 + 2/3)", 100, 0),
                 #volume,Tablespoons,Cubic_inches
-                ("x *  0.902344", 0, 0),
+                ("x *  0.902344", 100, 0),
                 #volume,Tablespoons,Cups
-                ("x * (1/16)", 0, 0),
+                ("x * (1/16)", 100, 0),
                 #volume,Tablespoons,Cubic_feet
-                ("x * 0.000522", 0, 0),
+                ("x * 0.000522", 100, 0),
                 #volume,Tablespoons,Gallons
-                ("x * 0.003906", 0, 0),
+                ("x * 0.003906", 100, 0),
                 #volume,Cubic_inches,Liters
-                ("x / 61.023744", 0, 0),
+                ("x / 61.023744", 100, 0),
                 #volume,Cubic_inches,Tablespoons
-                ("x /  0.902344", 0, 0),
+                ("x /  0.902344", 100, 0),
                 #volume,Cubic_inches,Cups
-                ("x * (1/16)", 0, 0),
+                ("x * (1/16)", 100, 0),
                 #volume,Cubic_inches,Cubic_feet
-                ("x * 0.000579", 0, 0),
+                ("x * 0.000579", 100, 0),
                 #volume,Cubic_inches,Gallons
-                ("x * 0.004329", 0, 0),
+                ("x * 0.004329", 100, 0),
                 #volume,Cups,Liters
-                ("x / 4.2267528377", 0, 0),
+                ("x / 4.2267528377", 100, 0),
                 #volume,Cups,Tablespoons
-                ("x / (1/16)", 0, 0),
+                ("x / (1/16)", 100, 0),
                 #volume,Cups,Cubic_inches
-                ("x / (1/16)", 0, 0),
+                ("x / (1/16)", 100, 0),
                 #volume,Cups,Cubic_feet
-                ("x * 0.008355", 0, 0),
+                ("x * 0.008355", 100, 0),
                 #volume,Cups,Gallons
-                ("x * 0.0625", 0, 0),
+                ("x * 0.0625", 100, 0),
                 #volume,Cubic_feet,Liters
-                ("x / 0.035315", 0, 0),
+                ("x / 0.035315", 100, 0),
                 #volume,Cubic_feet,Tablespoons
-                ("x / 0.000522", 0, 0),
+                ("x / 0.000522", 100, 0),
                 #volume,Cubic_feet,Cubic_inches
-                ("x / 0.000579", 0, 0),
+                ("x / 0.000579", 100, 0),
                 #volume,Cubic_feet,Cups
-                ("x / 0.008355", 0, 0),
+                ("x / 0.008355", 100, 0),
                 #volume,Cubic_feet,Gallons
-                ("x * 7.480519", 0, 0),
+                ("x * 7.480519", 100, 0),
                 #volume,Gallons,Liters
-                ("x / 0.264172", 0, 0),
+                ("x / 0.264172", 100, 0),
                 #volume,Gallons,Tablespoons
-                ("x / 0.003906", 0, 0),
+                ("x / 0.003906", 100, 0),
                 #volume,Gallons,Cubic_inches
-                ("x / 0.004329", 0, 0),
+                ("x / 0.004329", 100, 0),
                 #volume,Gallons,Cups
-                ("x / 0.0625", 0, 0),
+                ("x / 0.0625", 100, 0),
                 #volume,Gallons,Cubic_feet
-                ("x / 7.480519", 0, 0)
+                ("x / 7.480519", 100, 0)
             ])
     def test_convert(self, fn: str, value: float, converted: float):
 
