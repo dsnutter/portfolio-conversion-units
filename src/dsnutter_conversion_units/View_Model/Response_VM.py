1ec8cf5901e1507b9162f6f81031183f3d3a1bd8
DS Nutter <11504386+dsnutter@users.noreply.github.com>
2023-11-19 13:24:09 -0500

    fixed bug where student does not have the option for a value below absolute zero or negative volumes

--- src/dsnutter_conversion_units/View_Model/Response_VM.py
+++ src/dsnutter_conversion_units/View_Model/Response_VM.py
@@ -52,14 +52,12 @@ class Response_VM(Base_VM):
             hashmap['grade'] = grade
             if self._conversion_functions['filter'] is not None:
                 # judge if there are any additional requirments for the conversion that could make the
-                #   final conversion invalid
+                #   final conversion invalid, so am making this an array since maybe in the future we 
+                #   could support more than one filter configuration for a give question/conversion type
                 filter_result_msg = []
-                temp = self._conversion_functions['filter'](hashmap['from_type'], hashmap['input_value'])
-                if temp is not None:
-                    filter_result_msg.append(temp)
-                temp = self._conversion_functions['filter'](hashmap['to_type'], hashmap['response'])
-                if temp is not None:
-                    filter_result_msg.append(temp)
+                temp_filter = self._conversion_functions['filter'](hashmap['from_type'], hashmap['input_value'])
+                if temp_filter is not None:
+                    filter_result_msg.append(temp_filter)
             else:
                 filter_result_msg = None
         else:
