23485e3732189978159438b9662bf7225293a3e2
DS Nutter <11504386+dsnutter@users.noreply.github.com>
2023-11-29 09:09:19 -0500

    fixes to reading config files inside the python package module

--- src/portfolio_conversion_units/helpers/settings.py
+++ src/portfolio_conversion_units/helpers/settings.py
@@ -1,9 +1,14 @@
 from ..di.Configurations import Configurations
 from .Enums import BackendTypes
+import pkg_resources
 
-conversions_filename = 'configuration/conversions_config.csv'
-responses_filename = 'configuration/responses_config.csv'
-conversions_filter_filename = 'configuration/conversions_filter.csv'
+conversions_filename = pkg_resources.resource_filename(__name__, '../configuration/conversions_config.csv')
+responses_filename = pkg_resources.resource_filename(__name__, '../configuration/responses_config.csv')
+conversions_filter_filename = pkg_resources.resource_filename(__name__, '../configuration/conversions_filter.csv')
+
+print(conversions_filename)
+print(responses_filename)
+print(conversions_filter_filename)
 
 file_type = BackendTypes.CSV
 
